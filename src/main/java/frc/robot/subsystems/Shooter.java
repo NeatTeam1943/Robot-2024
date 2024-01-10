fe83bcda537bc69d7bb9f605b91fe36179a34f25
Gleb Kalukin <glebkalukin@gmail.com>
2024-01-10 18:59:13 +0200

    src: subsystem: Add Shooter

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+  /** Creates a new Shooter. */
+  public Shooter() {}
+
+  public void setMotorSpeed(double speed){
+
+  }
+
+  public void setAngle(double angle){
+
+  }
+
+  public double GetRPM(){
+
+    return 0;// TODO: give an actual number or something
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
