c9c507f9bc7d03e1795fae8801ed0ab357534d16
ErezAmirSolomon <salmonerez@gmail.com>
2024-01-11 17:16:52 +0200

    src:subsystems: WIP

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,11 +4,28 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveTrainConstants;
 
 public class DriveTrain extends SubsystemBase {
-  /** Creates a new DriveTrain. */
-  public DriveTrain() {}
+  private TalonFX m_frontRight;
+  private TalonFX m_frontLeft;
+  private TalonFX m_rearRight;
+  private TalonFX m_rearLeft;
+  private DifferentialDrive m_differentialDrive;
+
+  public DriveTrain() {
+    m_frontLeft = new TalonFX(DriveTrainConstants.frontLeftPort);
+    m_frontRight = new TalonFX(DriveTrainConstants.frontRightPort);
+    m_rearLeft = new TalonFX(DriveTrainConstants.rearLeftPort);
+    m_rearRight = new TalonFX(DriveTrainConstants.rearRightPort);
+
+
+    m_differentialDrive = new DifferentialDrive(m_frontLeft, m_frontRight);
+  }
 
   public void move(double moveSpeed, double rotationSpeed){
 
