43c3936fe3208284a5a45257dc31c4e7e13c36ff
LiranBratt2121 <brattlirannin@gmail.com>
2024-01-12 03:02:14 +0200

    Shooter: Remove redundant periodic function

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,13 +1,8 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-  /** Creates a new Shooter. */
   public Shooter() {}
 
   public void setMotorSpeed(double speed){
@@ -21,10 +16,5 @@ public class Shooter extends SubsystemBase {
   public double GetRPM(){
 
     return 0;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
