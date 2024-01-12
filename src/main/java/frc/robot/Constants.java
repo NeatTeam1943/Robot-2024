fb2ad03c0d86e38dea899fba19c1c457388591ca
LiranBratt2121 <brattlirannin@gmail.com>
2024-01-12 02:59:29 +0200

    Constants: Add DriveTrainConstants class with drivetrain dependencies

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,17 +1,5 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
@@ -22,9 +10,14 @@ public final class Constants {
   }
 
   public static class DriveTrainConstants {
-    public static final int frontRightPort = 0;
-    public static final int frontLeftPort = 1;
-    public static final int rearRightPort = 2;
-    public static final int rearLeftPort = 3;
+    public static final int kLeftFront = 1;
+    public static final int kLeftRear = 2;
+    public static final int kRightFront = 3;
+    public static final int kRightRear = 4;
+
+    public static final double kGearRatio = 10.71 / 1;
+    public static final double kWheelDiameterMeters = MeasurementConstants.kInchesToMeters * 6;
+
+    public static final double kEncoderSensorRotationsToMeters = Math.PI * kWheelDiameterMeters / kGearRatio;
   }
 }
