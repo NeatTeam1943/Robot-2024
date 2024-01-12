8a8d5539717ecf8ff5cd731a0767b9e032e05b95
LiranBratt2121 <brattlirannin@gmail.com>
2024-01-12 03:01:29 +0200

    Intake: Remove redundant periodic function

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,26 +1,17 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {}
+  public Intake() {
+  }
 
-  public void setMotorSpeed(double speed){
+  public void setMotorSpeed(double speed) {
 
   }
 
-  public boolean gotNote(){
+  public boolean gotNote() {
 
     return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
