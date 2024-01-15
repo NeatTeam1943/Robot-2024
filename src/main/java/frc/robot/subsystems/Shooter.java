1ae28fbad5d5ac52ec43aca568cc0378535ae3aa
Erez Solomon <salmonerez@gmail.com>
2024-01-15 19:02:47 +0200

    src: subsystems: Shooter: Add comments

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,17 +2,32 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/*
+ * The Shooter subsystem controls the robot's shooting mechanism.
+ */
 public class Shooter extends SubsystemBase {
+  /*
+   * Constructs the Shooter subsystem with motor controllers.
+   */
   public Shooter() {}
 
+  /*
+   * Sets the speed of the motors for initiating the shooting mechanism.
+   */
   public void setMotorSpeed(double speed){
 
   }
 
+  /*
+   * Sets the pitch angle of the shooting mechanism.
+   */
   public void setAngle(double angle){
 
   }
 
+  /*
+   * Returns the RPM of the motor.
+   */
   public double GetRPM(){
 
     return 0;
