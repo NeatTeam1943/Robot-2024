211b67ffbcd67e98ca00f625b702c5093bf0ecb3
Erez Solomon <salmonerez@gmail.com>
2024-01-15 18:48:30 +0200

    src: subsystems: Add comments

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,14 +2,28 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/*
+ * The Intake subsystem controls the robot's drive system.
+ */
 public class Intake extends SubsystemBase {
+
+  /**
+   * Constructs the DriveTrain subsystem with motor controllers and sets up
+   * follower behavior.
+   */
   public Intake() {
   }
 
+  /*
+   * Sets the speed of the motors for initiating the driving mechanism.
+   */
   public void setMotorSpeed(double speed) {
 
   }
 
+  /*
+   * Returns if the note has been acquired.
+   */
   public boolean gotNote() {
 
     return false;
