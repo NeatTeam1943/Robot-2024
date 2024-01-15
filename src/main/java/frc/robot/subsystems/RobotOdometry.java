ae904aa2e5ef41fde94c48996da3e210aa4e3feb
Erez Solomon <salmonerez@gmail.com>
2024-01-15 21:09:56 +0200

    src: subsystems: RobotOdometry: Add comments

--- src/main/java/frc/robot/subsystems/RobotOdometry.java
+++ src/main/java/frc/robot/subsystems/RobotOdometry.java
@@ -4,72 +4,120 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/*
+ * RobotOdometry contains data from spatial sensors.
+ */
 public class RobotOdometry extends SubsystemBase {
+  /*
+   * Constructs RobotOdometry subsystem.
+   */
   public RobotOdometry() {
   }
 
+  /*
+   * Sets position of robot in 2d field.
+   */
   public void setPosition2D(Pose2d position) {
 
   }
 
+  /*
+   * Gets position of robot in 2d field.
+   */
   public Pose2d getPosition2D() {
 
     return null;
   }
 
+  /*
+   * Sets position of robot in 3d field.
+   */
   public void setPosition3D(Pose3d position) {
 
   }
 
+  /*
+   * Gets position of robot in 3d field.
+   */
   public Pose2d getPosition3D() {
 
     return null;
   }
 
+  /*
+   * Sets heading of robot relative to a selected heading.
+   */
   public void setHeading(double angle) {
 
   }
 
+  /*
+   * Gets heading of robot relative to a selected heading.
+   */
   public double getHeading() {
 
     return 0;
   }
 
+  /*
+   * Gets the distance of the robot from an AprilTag.
+   */
   public double getDistanceToAT() {
 
     return 0;
   }
 
+  /*
+   * Gets the difference of the pitch of the robot from that of the AprilTag using lime lite.
+   */
   public double getPitchToAT() {
 
     return 0;
   }
 
+  /*
+   * Gets the difference of the yaw of the robot from that of the AprilTag using lime lite.
+   */
   public double getYawToAT() {
 
     return 0;
   }
 
+  /*
+   * Gets the distance of the robot from a Note using realsence.
+   */
   public double getDistanceToNote() {
 
     return 0;
   }
 
+  /*
+   * Gets the angle of the robot from a Note using realsence.
+   */
   public double getAngleToNote() {
 
     return 0;
   }
 
+  /*
+   * Gets the velocity of the robot using IMU.
+   */
   public double getVelocity() {
 
     return 0;
   }
 
+  /*
+   * Gets the acceleration of the robot using IMU.
+   */
   public double getAcceleration() {
 
     return 0;
   }
 
+  /*
+   * Gets the position of a Note inside the robot using a time-of-flight sensor.
+   */
   public double getDistanceTOF() {
 
     return 0;
