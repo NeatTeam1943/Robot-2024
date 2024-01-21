91ff8348e9898de3bcb28879feb4b64ccacd03b1
Alon Grinfeld <alon.grinfeld08@gmail.com>
2024-01-21 18:23:31 +0200

    src: subsystems: Shooter: Improve naming. Remove redundant functions. Add set angle speed function.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,67 +11,40 @@ import frc.robot.Constants.ShooterConstants;
  * The Shooter subsystem controls the robot's shooting mechanism.
  */
 public class Shooter extends SubsystemBase {
-  TalonFX m_leftShooter;
-  TalonFX m_rightShooter;
-
-  TalonFX m_angler;
+  TalonFX m_leftShooterMotor;
+  TalonFX m_rightShooterMotor;
+  TalonFX m_angleMotor;
 
   AnalogPotentiometer m_pot;
 
   DigitalInput m_topLimitSwitch;
   DigitalInput m_bottomLimitSwitch;
 
-  boolean m_inverted;
-
   /*
    * Constructs the Shooter subsystem with motor controllers.
    */
   public Shooter() {
-    m_leftShooter = new TalonFX(ShooterConstants.kLeftShooter);
-    m_rightShooter = new TalonFX(ShooterConstants.kRightShooter);
+    m_leftShooterMotor = new TalonFX(ShooterConstants.kLeftShooter);
+    m_rightShooterMotor = new TalonFX(ShooterConstants.kRightShooter);
 
-    m_angler = new TalonFX(ShooterConstants.kAngler);
+    m_angleMotor = new TalonFX(ShooterConstants.kAngler);
 
     m_pot = new AnalogPotentiometer(0, 180, ShooterConstants.kMinAngle);
-    
+
     m_topLimitSwitch = new DigitalInput(ShooterConstants.kTopLimitSwitchPort);
     m_bottomLimitSwitch = new DigitalInput(ShooterConstants.kBottomLimitSwitchPort);
-
-    m_inverted = false;
   }
 
   /*
    * Sets the speed of the motors for initiating the shooting mechanism.
    */
   public void setShooterMotorSpeed(double speed) {
-    m_leftShooter.set(speed);
-    m_rightShooter.set(speed);
+    m_leftShooterMotor.set(speed);
+    m_rightShooterMotor.set(speed);
   }
 
-  /*
-   * Toggles between a state and its inversion; used for determining the Angler orientation.
-   */
-  public void toggleInverted() {
-    m_inverted = !m_inverted;
-  }
-
-  /*
-   * Returns the speed of the Angler motor and inverts its orientation if needed.
-   */
-  public double getAnglerSpeed() {
-    double speed = m_angler.get();
-    if (m_inverted) {
-      speed *= -1;
-    }
-    return speed;
-  } 
-
-  /*
-   * Sets the orientation of the Angler motor.
-   */
-  public void setAnglerOrientation(boolean orientation) {
-    double speed = getAnglerSpeed();
-    m_angler.set(speed);
+  public void setAngleSpeed(double speed) {
+    m_angleMotor.set(speed);
   }
 
   /*
@@ -92,13 +65,13 @@ public class Shooter extends SubsystemBase {
    * Returns the RPM of the right motor.
    */
   public double getLeftRPM() {
-    return m_leftShooter.getVelocity().getValueAsDouble() * 60;
+    return m_leftShooterMotor.getVelocity().getValueAsDouble() * 60;
   }
 
   /*
    * Returns the RPM of the right motor.
    */
   public double getRightRPM() {
-    return m_rightShooter.getVelocity().getValueAsDouble() * 60;
+    return m_rightShooterMotor.getVelocity().getValueAsDouble() * 60;
   }
 }
