cf0f1baa75c6dd8bcc0f895dc76e188a2573edee
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-02 20:45:54 +0200

    Limelight: Add distance estimation function.

--- src/main/java/frc/robot/Vision/Limelight.java
+++ src/main/java/frc/robot/Vision/Limelight.java
@@ -1,3 +1,28 @@
+package frc.robot.Vision;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.Constants.MeasurementConstants;
+import frc.robot.Constants.FieldConstants;
+
+public class Limelight {
+    public enum Target {
+        AMP(FieldConstants.kAmpATHeightMeters),
+        SPEAKER(0), // TEMP VALUE
+        HUMAN_PLAYER(0); // TEMP VALUE
+
+        private double m_goalHeight;
+
+        private Target(double goalHeight) {
+            m_goalHeight = goalHeight;
+        }
+
+        public double getGoalHeight() {
+            return m_goalHeight;
+        }
+    }
+
     public void showAxes() {
         double yaw = LimelightHelpers.getTX(VisionConstants.kLimelightName);
         double pitch = LimelightHelpers.getTY(VisionConstants.kLimelightName);
@@ -30,3 +55,21 @@
                 .getDouble(0);
     }
 
+    public double getDistanceFrom(Target target) {
+        double targetOffsetAngle_Vertical = LimelightHelpers.getTY(VisionConstants.kLimelightName);
+
+        double limelightMountAngleDegrees = VisionConstants.kLimelightPitch;
+
+        double limelightLensHeightInches = VisionConstants.kLimelightHeightMeters * MeasurementConstants.kMeterToInches;
+
+        double goalHeightInches = target.getGoalHeight() * MeasurementConstants.kMeterToInches;
+
+        double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+        double angleToGoalRadians = angleToGoalDegrees * (MeasurementConstants.kDegToRad);
+
+        double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)
+                / Math.tan(angleToGoalRadians);
+
+        return distanceFromLimelightToGoalInches * MeasurementConstants.kInchesToMeters;
+    }
+}
