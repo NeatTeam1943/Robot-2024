814305888da67acd9fb5fcddc4a5d9001a09ba49
Erez Solomon <salmonerez@gmail.com>
2024-02-04 19:11:10 +0200

    src: command: ReachPitch: Add ReachPitch

---
+++ src/main/java/frc/robot/commands/ReachPitch.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.BangBangController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Pitcher;
+
+/**
+ * The ReachPitch command reaches the desired pitch-angle of the shooting-mechanism.
+ * 
+ * @param pitcher - the subsystem which will be used.
+ * 
+ * @param desiredPitch - the desired pitch-angle of the shooting-mechanism.
+ */
+public class ReachPitch extends Command {
+  Pitcher m_pitcher;
+
+  BangBangController m_controller;
+  double m_desiredPitch;
+  boolean m_reachedPitch;
+
+  /** Creates a new ReachPitch. */
+  public ReachPitch(Pitcher pitcher, double desiredPitch) {
+    m_pitcher = pitcher;
+
+    m_controller = new BangBangController();
+    m_desiredPitch = desiredPitch;
+    m_reachedPitch = false;
+
+    addRequirements(m_pitcher);
+  }
+
+  @Override
+  public void initialize() {}
+
+  /**
+   * Sets the speed of the angle-motors using BangBangControl; checks if the pitch-angle has been reached.
+   */
+  @Override
+  public void execute() {
+    m_pitcher.setAngleMotorsSpeed(m_controller.calculate(m_pitcher.getAngle(), m_desiredPitch));
+
+    if (m_pitcher.getAngle() == m_desiredPitch) {
+      m_reachedPitch = true;
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  /**
+   * @return If the pitch-angle has been reached.
+   */
+  @Override
+  public boolean isFinished() {
+    return m_reachedPitch;
+  }
+}
