21f6449fd1c51159054cfcd1a33c98fd7090adb8
Erez Solomon <salmonerez@gmail.com>
2024-02-04 04:26:55 +0200

    src: subsystems: Shooter: Remove limit switches for redundancy; add access-modifiers

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,19 +1,15 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
 /**
- * The Shooter subsystem controls the robot's shooting mechanism.
+ * The Shooter subsystem controls the robot's shooting-mechanism.
  */
 public class Shooter extends SubsystemBase {
-  TalonFX m_leftShooterMotor;
-  TalonFX m_rightShooterMotor;
-
-  DigitalInput m_topLimitSwitch;
-  DigitalInput m_bottomLimitSwitch;
+  private TalonFX m_leftShooterMotor;
+  private TalonFX m_rightShooterMotor;
 
   /**
    * Constructs the Shooter subsystem with motor controllers and sensors.
@@ -28,7 +24,7 @@ public class Shooter extends SubsystemBase {
    * 
    * @param speed - the speed at which the shooter motors will rotate.
    */
-  public void setShooterMotorSpeed(double speed) {
+  public void setShooterMotorsSpeed(double speed) {
     m_leftShooterMotor.set(speed);
     m_rightShooterMotor.set(speed);
   }
