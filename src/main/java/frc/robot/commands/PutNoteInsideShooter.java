d628d017b241a300ef0ff077b9a99c47bf81377a
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-11 22:08:11 +0200

    PutNoteInsideShooter: Add formatting

--- src/main/java/frc/robot/commands/PutNoteInsideShooter.java
+++ src/main/java/frc/robot/commands/PutNoteInsideShooter.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -22,34 +18,32 @@ public class PutNoteInsideShooter extends Command {
    */
   public PutNoteInsideShooter(Transport transport) {
     m_transport = transport;
+
     m_frontOfTheNotePassedTheSwitch = false;
     m_backOfTheNotePassedTheSwitch = false;
+
     addRequirements(transport);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     m_transport.setBeltsSpeed(TransportConstants.kBeltsSpeed);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     if (!m_frontOfTheNotePassedTheSwitch){
-      m_frontOfTheNotePassedTheSwitch = !m_transport.isSwitchPressed();
+      m_frontOfTheNotePassedTheSwitch = !m_transport.isNoteVisible();
     } else {
-      m_backOfTheNotePassedTheSwitch = m_transport.isSwitchPressed();
+      m_backOfTheNotePassedTheSwitch = m_transport.isNoteVisible();
     }
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_transport.setBeltsSpeed(0);
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return m_backOfTheNotePassedTheSwitch;
