59471c04169842a971001c1f7d7ff076437e6f39
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-13 18:28:41 +0200

    Constant: Update `PID` values to temporary values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,9 +41,9 @@ public final class Constants {
     public static final int kLeftShooterMotor = 8;
     public static final int kRightShooterMotor = 9;
     
-    public static final double kp = -1;
-    public static final double ki = -1;
-    public static final double kd = -1;
+    public static final double kP = 0.5; // TODO Add real values
+    public static final double kI = 0;
+    public static final double kD = 0;
   }
 
   public static class PitcherConstants {
