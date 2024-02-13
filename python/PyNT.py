7527873d8f381b4b2118bd01e084e11fef0d8149
Venus <zivrada2221@gmail.com>
2024-02-13 17:54:27 +0200

    python: Rename values

--- python/PyNT.py
+++ python/PyNT.py
@@ -7,25 +7,25 @@ TABLE = INST.getTable("RealSense Data")
 screenX = TABLE.getDoubleTopic("ScreenX")
 screenY = TABLE.getDoubleTopic("ScreenY")
 
-targetPitch = TABLE.getDoubleTopic("TargetPitch")
-targetRoll = TABLE.getDoubleTopic("TargetRoll")
-targetYaw = TABLE.getDoubleTopic("TargetYaw")
+target_pitch = TABLE.getDoubleTopic("TargetPitch")
+target_roll = TABLE.getDoubleTopic("TargetRoll")
+target_yaw = TABLE.getDoubleTopic("TargetYaw")
 
-optimalVelocity = TABLE.getDoubleTopic("Optimal Shooter Velocity")
-shooterAngle = TABLE.getDoubleTopic("Optimal Shooter Angle")
+optimal_velocity = TABLE.getDoubleTopic("Optimal Shooter Velocity")
+shooter_angle = TABLE.getDoubleTopic("Optimal Shooter Angle")
 deltaX = TABLE.getDoubleTopic("DeltaX")
 
 # Set up the publishers
-screenXPublisher = screenX.publish()
-screenYPublisher = screenY.publish()
+screenX_publisher = screenX.publish()
+screenY_publisher = screenY.publish()
 
-targetPitchPublisher = targetPitch.publish()
-targetRollPublisher = targetRoll.publish()
-targetYawPublisher = targetYaw.publish()
+target_pitch_publisher = target_pitch.publish()
+target_roll_publisher = target_roll.publish()
+target_yaw_publisher = target_yaw.publish()
 
-optimalVelocityPublisher = optimalVelocity.publish()
-shooterAnglePublisher = shooterAngle.publish()
-deltaXPublisher = deltaX.publish()
+optimal_velocity_publisher = optimal_velocity.publish()
+shooter_angle_publisher = shooter_angle.publish()
+deltaX_publisher = deltaX.publish()
 
 INST.startClient4("Test")
 INST.setServer(1943) # where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
@@ -37,22 +37,22 @@ while True:
     screenX = 0.0
     screenY = 0.0
 
-    targetPitch = 0.0
-    targetRoll = 0.0
-    targetYaw = 0.0
+    target_pitch = 0.0
+    target_roll = 0.0
+    target_yaw = 0.0
 
-    optimalVelocity = 0.0
-    shooterAngle = 0.0
+    optimal_velocity = 0.0
+    shooter_angle = 0.0
     deltaX = 0.0
 
     # Publish the data
-    screenXPublisher.setDouble(screenX)
-    screenYPublisher.setDouble(screenY)
+    screenX_publisher.setDouble(screenX)
+    screenY_publisher.setDouble(screenY)
 
-    targetPitchPublisher.setDouble(targetPitch)
-    targetRollPublisher.setDouble(targetRoll)
-    targetRollPublisher.setDouble(targetYaw)
+    target_pitch_publisher.setDouble(target_pitch)
+    target_roll_publisher.setDouble(target_roll)
+    target_roll_publisher.setDouble(target_yaw)
 
-    optimalVelocityPublisher.setDouble(optimalVelocity)
-    shooterAnglePublisher.setDouble(shooterAngle)
-    deltaXPublisher.setDouble(deltaX)
+    optimal_velocity_publisher.setDouble(optimal_velocity)
+    shooter_angle_publisher.setDouble(shooter_angle)
+    deltaX_publisher.setDouble(deltaX)
