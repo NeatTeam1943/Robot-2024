fa6e542c58fd4edc3ff33a90c5a7ca9d2ebf9716
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-17 22:16:34 +0200

    TransportToShoot: Add a general stopping cond

--- src/main/java/frc/robot/commands/routines/TransportToShoot.java
+++ src/main/java/frc/robot/commands/routines/TransportToShoot.java
@@ -1,14 +1,18 @@
 package frc.robot.commands.routines;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import frc.robot.commands.HeadingCommands.IsInMode;
 import frc.robot.commands.transportationCommands.TransportNote;
+import frc.robot.general.RobotHeading;
 import frc.robot.subsystems.NetworkTables;
 import frc.robot.subsystems.Pitcher;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Transport;
 
-public class TransportToShot extends ParallelCommandGroup {
-  public TransportToShot(Pitcher pitcher, Shooter shooter, Transport transport, NetworkTables nt) {
+public class TransportToShoot extends ParallelDeadlineGroup {
+  public TransportToShoot(Pitcher pitcher, Shooter shooter, Transport transport, NetworkTables nt) {
+    super(new IsInMode(RobotHeading.SHOOTER));
+    
     addCommands(
         new ShooterVision(pitcher, shooter, nt),
         new TransportNote(transport));
