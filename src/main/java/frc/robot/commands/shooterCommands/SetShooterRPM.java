6f3147857449c2baedec62d9cf2ef4384cacdd86
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-17 21:27:53 +0200

    SetShooterRPM: Add initialize function to set a setpoint

--- src/main/java/frc/robot/commands/shooterCommands/SetShooterRPM.java
+++ src/main/java/frc/robot/commands/shooterCommands/SetShooterRPM.java
@@ -6,8 +6,18 @@ import frc.robot.subsystems.Shooter;
  * Sets desired RPM for the shooter
  */
 public class SetShooterRPM extends SetShooterRPMBase {
+  private double m_setpoint;
+
   public SetShooterRPM(Shooter shooter, double setpoint) {
-    super(shooter, setpoint);
+    super(shooter);
+
+    m_setpoint = setpoint;
+  }
+
+  @Override
+  public void initialize() {
+    m_leftController.setSetpoint(m_setpoint);
+    m_rightController.setSetpoint(m_setpoint);
   }
 
   @Override
