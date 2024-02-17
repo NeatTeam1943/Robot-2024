90d0862fad1e76a091217a40382727cd6c57ac98
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-17 21:32:27 +0200

    SetShooterToDefaultRPM: Utilize SetShooterRPM to make the class more concise

--- src/main/java/frc/robot/commands/shooterCommands/SetShooterToDefaultRPM.java
+++ src/main/java/frc/robot/commands/shooterCommands/SetShooterToDefaultRPM.java
@@ -7,12 +7,12 @@ import frc.robot.subsystems.Shooter;
 /**
  * Sets desired RPM for the shooter while not in shooting range.
  */
-public class SetShooterToDefaultRPM extends SetShooterRPMBase {
+public class SetShooterToDefaultRPM extends SetShooterRPM {
   private RobotOdometry m_odometry;
 
   public SetShooterToDefaultRPM(Shooter shooter, RobotOdometry odometry) {
     super(shooter, ShooterConstants.kDefaultRPM);
-    
+
     m_odometry = odometry;
   }
 
