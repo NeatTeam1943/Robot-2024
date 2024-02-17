ec95d3a230dccce824f192f18c34825027dfb598
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-17 16:05:20 +0200

    Add Intake mode initialization command

---
+++ src/main/java/frc/robot/commands/routines/InitializeIntakeMode.java
@@ -0,0 +1,20 @@
+package frc.robot.commands.routines;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants.IntakeModeConstants;
+import frc.robot.commands.pitcherCommands.ReachPitch;
+import frc.robot.commands.shooterCommands.SetShooterRPM;
+import frc.robot.subsystems.Pitcher;
+import frc.robot.subsystems.Shooter;
+
+/**
+ * - Sets the shooter's pitch to a default value.
+ * - Sets the shooter velocity to 0.
+ */
+public class InitializeIntakeMode extends ParallelCommandGroup {
+  public InitializeIntakeMode(Pitcher pitcher, Shooter shooter) {
+    addCommands(
+        new ReachPitch(pitcher, IntakeModeConstants.kDefaultPitcherAngle),
+        new SetShooterRPM(shooter, 0));
+  }
+}
