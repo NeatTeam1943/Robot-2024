022b424fed9ea523c98303c1955e4995ae66e171
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-17 21:41:47 +0200

    ShooterVision: Add a ParallelCommandGroup that sets up the pitch and the shooter's speed with regards to vision

---
+++ src/main/java/frc/robot/commands/routines/ShooterVision.java
@@ -0,0 +1,17 @@
+package frc.robot.commands.routines;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.pitcherCommands.ReachPitchVision;
+import frc.robot.commands.shooterCommands.SetVisionShooterRPM;
+import frc.robot.subsystems.NetworkTables;
+import frc.robot.subsystems.Pitcher;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterVision extends ParallelCommandGroup {
+  public ShooterVision(Pitcher pitcher, Shooter shooter, NetworkTables nt) {
+    addCommands(
+      new ReachPitchVision(pitcher, nt),
+      new SetVisionShooterRPM(shooter, nt)
+    );
+  }
+}
