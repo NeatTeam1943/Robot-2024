529d0b96a64be92d5388c77aabfc8af4312b358e
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-17 21:25:21 +0200

    SetShooterRPMBASE: Remove setpoint req

--- src/main/java/frc/robot/commands/shooterCommands/SetShooterRPMBase.java
+++ src/main/java/frc/robot/commands/shooterCommands/SetShooterRPMBase.java
@@ -7,24 +7,25 @@ import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.Shooter;
 
 /**
- * Abstract command for setting and maintaining a desired RPM for the shooter motors.
+ * Abstract command for setting and maintaining a desired RPM for the shooter
+ * motors.
  */
 public abstract class SetShooterRPMBase extends Command {
   protected Shooter m_shooter;
 
   protected PIDController m_leftController;
   protected PIDController m_rightController;
-  
+
   protected SimpleMotorFeedforward m_ff;
 
-  protected double m_setpoint;
-
-  public SetShooterRPMBase(Shooter shooter, double setpoint) {
-    m_setpoint = setpoint;
+  public SetShooterRPMBase(Shooter shooter) {
     m_shooter = shooter;
 
     m_leftController = new PIDController(ShooterConstants.kP, ShooterConstants.kI, ShooterConstants.kD);
     m_rightController = new PIDController(ShooterConstants.kP, ShooterConstants.kI, ShooterConstants.kD);
+
+    m_leftController.setTolerance(ShooterConstants.kLeftControllerThreshold);
+    m_rightController.setTolerance(ShooterConstants.kRightControllerThreshold);
 
     m_ff = new SimpleMotorFeedforward(ShooterConstants.kS, ShooterConstants.kV);
 
@@ -32,21 +33,15 @@ public abstract class SetShooterRPMBase extends Command {
   }
 
   @Override
-  public void initialize() {
-    m_leftController.setTolerance(ShooterConstants.kLeftControllerThreshold);
-    m_rightController.setTolerance(ShooterConstants.kRightControllerThreshold);
-
-    m_leftController.setSetpoint(m_setpoint);
-    m_rightController.setSetpoint(m_setpoint);
-  }
+  public void initialize() {}
 
   @Override
   public void execute() {
     m_shooter.setLeftMotorVoltage(
-      m_ff.calculate(m_shooter.getLeftRate()) + m_leftController.calculate(m_shooter.getLeftRPM()));
+        m_ff.calculate(m_shooter.getLeftRate()) + m_leftController.calculate(m_shooter.getLeftRPM()));
 
     m_shooter.setRightMotorVoltage(
-      m_ff.calculate(m_shooter.getRightRate()) + m_leftController.calculate(m_shooter.getRightRPM()));
+        m_ff.calculate(m_shooter.getRightRate()) + m_leftController.calculate(m_shooter.getRightRPM()));
   }
 
   @Override
