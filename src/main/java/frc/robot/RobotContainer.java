96a58ff99d1b5a215174a8a4187f5c2a3cd00b30
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-20 19:57:36 +0200

    RobotContainer: Setup subsystems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,19 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 public class RobotContainer {
   private final CommandXboxController m_driverController;
 
+  private final DriveTrain m_drive;
+  private final Pitcher m_pitcher;
+  private final Shooter m_shooter;
+  private final Intake m_intake;
+  private final Transport m_transport; 
+
   public RobotContainer() {
     m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    m_drive = new DriveTrain();
+    m_pitcher = new Pitcher(); 
+    m_shooter = new Shooter();
+    m_intake = new Intake();
+    m_transport = new Transport();
 
     configureBindings();
   }
