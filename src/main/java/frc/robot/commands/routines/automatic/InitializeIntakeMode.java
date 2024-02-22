9a45295a54e70b4e58c4bc27fffeb2144cada048
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-22 23:36:14 +0200

    Move to dir

--- src/main/java/frc/robot/commands/routines/automatic/InitializeIntakeMode.java
+++ src/main/java/frc/robot/commands/routines/automatic/InitializeIntakeMode.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.routines;
+package frc.robot.commands.routines.automatic;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.IntakeModeConstants;
--- src/main/java/frc/robot/commands/routines/automatic/InitializeShooterMode.java
+++ src/main/java/frc/robot/commands/routines/automatic/InitializeShooterMode.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.routines;
+package frc.robot.commands.routines.automatic;
 
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.Constants.ShooterModeConstants;
