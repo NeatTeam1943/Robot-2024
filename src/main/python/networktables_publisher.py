d4cdf8575b20c7274e85f3fbf106960b4bcca3ff
LiranBratt2121 <brattlirannin@gmail.com>
2024-02-22 16:24:26 +0200

    networktables_publisher: Add "topic" to variable names

--- src/main/python/networktables_publisher.py
+++ src/main/python/networktables_publisher.py
@@ -7,28 +7,28 @@ INST = ntcore.NetworkTableInstance.getDefault()
 TABLE = INST.getTable("RealSense Data")
 
 # Set up the topic
-screenX = TABLE.getDoubleTopic("ScreenX")
-screenY = TABLE.getDoubleTopic("ScreenY")
+screenX_topic = table.getDoubleTopic("ScreenX")
+screenY_topic = table.getDoubleTopic("ScreenY")
 
-target_pitch = TABLE.getDoubleTopic("TargetPitch")
-target_roll = TABLE.getDoubleTopic("TargetRoll")
-target_yaw = TABLE.getDoubleTopic("TargetYaw")
+target_pitch_topic = table.getDoubleTopic("TargetPitch")
+target_roll_topic = table.getDoubleTopic("TargetRoll")
+target_yaw_topic = table.getDoubleTopic("TargetYaw")
 
-optimal_velocity = TABLE.getDoubleTopic("Optimal Shooter Velocity")
-shooter_angle = TABLE.getDoubleTopic("Optimal Shooter Angle")
-deltaX = TABLE.getDoubleTopic("DeltaX")
+optimal_velocity_topic = table.getDoubleTopic("Optimal Shooter Velocity")
+shooter_angle_topic = table.getDoubleTopic("Optimal Shooter Angle")
+deltaX_topic = table.getDoubleTopic("DeltaX")
 
 # Set up the publishers
-screenX_publisher = screenX.publish()
-screenY_publisher = screenY.publish()
+screenX_publisher = screenX_topic.publish()
+screenY_publisher = screenY_topic.publish()
 
-target_pitch_publisher = target_pitch.publish()
-target_roll_publisher = target_roll.publish()
-target_yaw_publisher = target_yaw.publish()
+target_pitch_publisher = target_pitch_topic.publish()
+target_roll_publisher = target_roll_topic.publish()
+target_yaw_publisher = target_yaw_topic.publish()
 
-optimal_velocity_publisher = optimal_velocity.publish()
-shooter_angle_publisher = shooter_angle.publish()
-deltaX_publisher = deltaX.publish()
+optimal_velocity_publisher = optimal_velocity_topic.publish()
+shooter_angle_publisher = shooter_angle_topic.publish()
+deltaX_publisher = deltaX_topic.publish()
 
 INST.startClient4(CLIENT)
 INST.setServer(SERVER)
